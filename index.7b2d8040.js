aaf53369e8d4e2f99c444ad1eaf55fc277330b20
VitaliyKorzhevyi <111433531+VitaliyKorzhevyi@users.noreply.github.com>
2023-05-19 16:44:10 +0000

    Deploying to gh-pages from @ VitaliyKorzhevyi/Tyler-Herchian@0ed091a16fcd672b724b6e3c209190ba42170b7e 🚀

---
+++ index.7b2d8040.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("menu-toggle");document.querySelector(".logo");e.addEventListener("click",(function(){e.classList.toggle("open")}))}();
+//# sourceMappingURL=index.7b2d8040.js.map
---
+++ index.db3fa8ed.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("menu-toggle");document.querySelector(".logo");e.addEventListener("click",(function(){e.classList.toggle("open")}));
+//# sourceMappingURL=index.db3fa8ed.js.map
