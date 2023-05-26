6c6a2123012edc8decbed7155b87722357fada93
VitaliyKorzhevyi <111433531+VitaliyKorzhevyi@users.noreply.github.com>
2023-05-26 17:07:45 +0300

    g3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const menuToggle = document.getElementById('menu-toggle');
 const logoOpenMenu = document.querySelector('.logo');
+const bodyScrollLock = document.querySelector('body');
 
 
 menuToggle.addEventListener('click', onBtnToggleClass)
 function onBtnToggleClass() {
     menuToggle.classList.toggle('open');
+    bodyScrollLock.classList.toggle('no-scroll');
 }
