43f31f03a7f9c35e3aeb1cdebb35eceba16d5409
VitaliyKorzhevyi <111433531+VitaliyKorzhevyi@users.noreply.github.com>
2023-05-26 17:54:33 +0300

    r5

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const menuToggle = document.getElementById('menu-toggle');
-const logoOpenMenu = document.querySelector('.logo');
+const mobileMenu = document.querySelector('.backdrop-menu');
 const bodyScrollLock = document.querySelector('body');
 
 
 menuToggle.addEventListener('click', onBtnToggleClass)
 function onBtnToggleClass() {
     menuToggle.classList.toggle('open');
+    mobileMenu.classList.toggle('is-open');
     bodyScrollLock.classList.toggle('no-scroll');
 }
