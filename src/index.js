489082750ec6db00cf0c6d1bcd0830bd5614c5ca
VitaliyKorzhevyi <111433531+VitaliyKorzhevyi@users.noreply.github.com>
2023-05-29 13:06:35 +0300

    q3

--- src/index.js
+++ src/index.js
@@ -9,3 +9,28 @@ function onBtnToggleClass() {
     mobileMenu.classList.toggle('is-open');
     bodyScrollLock.classList.toggle('no-scroll');
 }
+
+const servicesList = document.querySelector('.services-list');
+const servicesLines = document.querySelectorAll('.services-lines span');
+
+servicesList.addEventListener('scroll', updateLinesHeight);
+window.addEventListener('load', updateLinesHeight);
+
+function updateLinesHeight() {
+  const scrollLeft = servicesList.scrollLeft;
+  const imageWidth = 245;
+  const gap = 30;
+  const lineHeight = 2;
+
+  const imageIndex = Math.floor((scrollLeft + gap / 2) / (imageWidth + gap));
+  const lineIndex = Math.max(0, Math.min(3, imageIndex));
+
+  servicesLines.forEach((line, index) => {
+    if (index === lineIndex) {
+      line.style.height = `${lineHeight}px`;
+    } else {
+      line.style.height = `1px`;
+    }
+  });
+}
+  
