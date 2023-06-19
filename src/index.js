fda6945a7ed24f6200b14797958f95e5c9f73689
VitaliyKorzhevyi <111433531+VitaliyKorzhevyi@users.noreply.github.com>
2023-06-19 06:52:58 +0300

    debug1

--- src/index.js
+++ src/index.js
@@ -10,22 +10,29 @@ AOS.init({
   once: true,
 });
 
-const forName = document.querySelector('#forName');
-const forEmail = document.querySelector('#forEmail');
-const formMessage = document.querySelector('#formMessage');
-const submitButton = document.querySelector('#submitForm');
+
 
 const handlerForm = e => {
   e.preventDefault();
+  const username = forName.value.trim();
+  const email = forEmail.value.trim();
+  const message = formMessage.value.trim();
+
+  // Проверяем, являются ли поля пустыми
+  if (!username || !email || !message) {
+    Notiflix.Notify.warning('Please fill in all fields.');
+    return; // Прекращаем выполнение функции, если есть пустые поля
+  }
+
   const formData = {
-    username: forName.value.trim(),
-    email: forEmail.value.trim(),
-    message: formMessage.value.trim(),
+    username: username,
+    email: email,
+    message: message,
   };
 
-  console.log(formData, 'formdat');
+  console.log(formData, 'formData');
 
-  fetch('http://localhost:5000', {
+  fetch('mail', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -33,24 +40,45 @@ const handlerForm = e => {
     body: JSON.stringify(formData),
   })
     .then(response => {
+      if (response.status === 422) {
+        throw new Error(422);
+      }
+
+      if (response.status === 429) {
+        throw new Error(429);
+      }
+      
       if (!response.ok) {
-        throw new Error('Error repons');
+        throw new Error('Error response');
       }
       console.log(response);
       return response.json();
     })
     .then(data => {
       Notiflix.Notify.success('Your message has been sent!');
+      
+      // Очищаем значения полей формы после успешной отправки
+      forName.value = '';
+      forEmail.value = '';
+      formMessage.value = '';
     })
     .catch(error => {
-      Notiflix.Notify.warning('Invalid data.');
+      if (error.message === 422) {
+        Notiflix.Notify.warning('Invalid date');
+      } else if (error.message === 429) {
+        Notiflix.Notify.warning('Please try again in 1 minute');
+      } else {
+        Notiflix.Notify.warning('Please try again later');
+      }
     });
-  forName.value = '';
-  forEmail.value = '';
-  formMessage.value = '';
 };
-submitButton.addEventListener('click', handlerForm);
 
+const forName = document.querySelector('#forName');
+const forEmail = document.querySelector('#forEmail');
+const formMessage = document.querySelector('#formMessage');
+const submitButton = document.querySelector('#submitForm');
+
+submitButton.addEventListener('click', handlerForm);
 
 const menuToggle = document.getElementById('menu-toggle');
 const mobileMenu = document.querySelector('.backdrop-menu');
@@ -99,3 +127,55 @@ window.matchMedia('(min-width: 320px)').addEventListener('change', e => {
   openMenuBtn.setAttribute('aria-expanded', false);
   bodyScrollLock.classList.remove('no-scroll');
 });
+
+
+const input = document.getElementById('forName');
+const validationMessage = document.getElementById('validationMessage');
+
+input.addEventListener('input', function () {
+  const inputValue = input.value;
+  if (inputValue.length < 3) {
+    validationMessage.textContent = 'Min. 3 characters';
+  } else {
+    validationMessage.textContent = '';
+  }
+  
+  if (inputValue.length === 0) {
+    validationMessage.textContent = '';
+  }
+});
+
+const emailInput = document.getElementById('forEmail');
+const emailValidationMessage = document.getElementById('emailValidationMessage');
+
+emailInput.addEventListener('input', function () {
+  const emailValue = emailInput.value;
+  if (emailValue.length === 0) {
+    emailValidationMessage.textContent = '';
+  } else if (!validateEmail(emailValue)) {
+    emailValidationMessage.textContent = 'Invalid email format';
+  } else {
+    emailValidationMessage.textContent = '';
+  }
+});
+
+const messageInput = document.getElementById('formMessage');
+const messageValidationMessage = document.getElementById('messageValidationMessage');
+
+messageInput.addEventListener('input', function () {
+  const messageValue = messageInput.value;
+  if (messageValue.length < 10) {
+    messageValidationMessage.textContent = 'Min. 10 characters';
+  } else {
+    messageValidationMessage.textContent = '';
+  }
+  
+  if (messageValue.length === 0) {
+    messageValidationMessage.textContent = '';
+  }
+});
+
+function validateEmail(email) {
+  const emailRegex = /\S+@\S+\.\S+/;
+  return emailRegex.test(email);
+}
